48230e031f3b90142902a2241c6cf41a23d115fb
Feliweed35 <felipe.lizana.veloz@gmail.com>
2024-10-01 17:15:16 -0300

    Add files via upload

---
+++ eva2/Form1.Designer.vb
@@ -0,0 +1,277 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtRUT = New System.Windows.Forms.TextBox()
+        Me.txtNombre = New System.Windows.Forms.TextBox()
+        Me.txtApellido = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.rbtnNoEspecifica = New System.Windows.Forms.RadioButton()
+        Me.rbtnFemenino = New System.Windows.Forms.RadioButton()
+        Me.rbtnMasculino = New System.Windows.Forms.RadioButton()
+        Me.txtCiudad = New System.Windows.Forms.TextBox()
+        Me.txtObservaciones = New System.Windows.Forms.TextBox()
+        Me.cmbComuna = New System.Windows.Forms.ComboBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.btnBuscar = New System.Windows.Forms.Button()
+        Me.btnGuardar = New System.Windows.Forms.Button()
+        Me.btnModificar = New System.Windows.Forms.Button()
+        Me.btnEliminar = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'txtRUT
+        '
+        Me.txtRUT.Location = New System.Drawing.Point(189, 30)
+        Me.txtRUT.Name = "txtRUT"
+        Me.txtRUT.Size = New System.Drawing.Size(100, 20)
+        Me.txtRUT.TabIndex = 0
+        '
+        'txtNombre
+        '
+        Me.txtNombre.Location = New System.Drawing.Point(189, 77)
+        Me.txtNombre.Name = "txtNombre"
+        Me.txtNombre.Size = New System.Drawing.Size(100, 20)
+        Me.txtNombre.TabIndex = 1
+        '
+        'txtApellido
+        '
+        Me.txtApellido.Location = New System.Drawing.Point(189, 128)
+        Me.txtApellido.Name = "txtApellido"
+        Me.txtApellido.Size = New System.Drawing.Size(100, 20)
+        Me.txtApellido.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(89, 33)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(27, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Rut:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(89, 84)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(52, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Nombres:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(89, 135)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(55, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Apellidos: "
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.rbtnNoEspecifica)
+        Me.GroupBox1.Controls.Add(Me.rbtnFemenino)
+        Me.GroupBox1.Controls.Add(Me.rbtnMasculino)
+        Me.GroupBox1.Location = New System.Drawing.Point(92, 167)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(539, 100)
+        Me.GroupBox1.TabIndex = 6
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Sexo: "
+        '
+        'rbtnNoEspecifica
+        '
+        Me.rbtnNoEspecifica.AutoSize = True
+        Me.rbtnNoEspecifica.Location = New System.Drawing.Point(272, 43)
+        Me.rbtnNoEspecifica.Name = "rbtnNoEspecifica"
+        Me.rbtnNoEspecifica.Size = New System.Drawing.Size(90, 17)
+        Me.rbtnNoEspecifica.TabIndex = 2
+        Me.rbtnNoEspecifica.TabStop = True
+        Me.rbtnNoEspecifica.Text = "No especifica"
+        Me.rbtnNoEspecifica.UseVisualStyleBackColor = True
+        '
+        'rbtnFemenino
+        '
+        Me.rbtnFemenino.AutoSize = True
+        Me.rbtnFemenino.Location = New System.Drawing.Point(140, 43)
+        Me.rbtnFemenino.Name = "rbtnFemenino"
+        Me.rbtnFemenino.Size = New System.Drawing.Size(71, 17)
+        Me.rbtnFemenino.TabIndex = 1
+        Me.rbtnFemenino.TabStop = True
+        Me.rbtnFemenino.Text = "Femenino"
+        Me.rbtnFemenino.UseVisualStyleBackColor = True
+        '
+        'rbtnMasculino
+        '
+        Me.rbtnMasculino.AutoSize = True
+        Me.rbtnMasculino.Location = New System.Drawing.Point(17, 43)
+        Me.rbtnMasculino.Name = "rbtnMasculino"
+        Me.rbtnMasculino.Size = New System.Drawing.Size(73, 17)
+        Me.rbtnMasculino.TabIndex = 0
+        Me.rbtnMasculino.TabStop = True
+        Me.rbtnMasculino.Text = "Masculino"
+        Me.rbtnMasculino.UseVisualStyleBackColor = True
+        '
+        'txtCiudad
+        '
+        Me.txtCiudad.Location = New System.Drawing.Point(189, 311)
+        Me.txtCiudad.Name = "txtCiudad"
+        Me.txtCiudad.Size = New System.Drawing.Size(121, 20)
+        Me.txtCiudad.TabIndex = 7
+        '
+        'txtObservaciones
+        '
+        Me.txtObservaciones.Location = New System.Drawing.Point(189, 352)
+        Me.txtObservaciones.Name = "txtObservaciones"
+        Me.txtObservaciones.Size = New System.Drawing.Size(121, 20)
+        Me.txtObservaciones.TabIndex = 8
+        '
+        'cmbComuna
+        '
+        Me.cmbComuna.FormattingEnabled = True
+        Me.cmbComuna.Location = New System.Drawing.Point(189, 274)
+        Me.cmbComuna.Name = "cmbComuna"
+        Me.cmbComuna.Size = New System.Drawing.Size(121, 21)
+        Me.cmbComuna.TabIndex = 9
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(95, 277)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(49, 13)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "Comuna:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(95, 314)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(40, 13)
+        Me.Label5.TabIndex = 11
+        Me.Label5.Text = "Ciudad"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(95, 359)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(63, 13)
+        Me.Label6.TabIndex = 12
+        Me.Label6.Text = "Descripcion"
+        '
+        'btnBuscar
+        '
+        Me.btnBuscar.Location = New System.Drawing.Point(364, 53)
+        Me.btnBuscar.Name = "btnBuscar"
+        Me.btnBuscar.Size = New System.Drawing.Size(75, 23)
+        Me.btnBuscar.TabIndex = 13
+        Me.btnBuscar.Text = "Buscar"
+        Me.btnBuscar.UseVisualStyleBackColor = True
+        '
+        'btnGuardar
+        '
+        Me.btnGuardar.Location = New System.Drawing.Point(364, 314)
+        Me.btnGuardar.Name = "btnGuardar"
+        Me.btnGuardar.Size = New System.Drawing.Size(127, 23)
+        Me.btnGuardar.TabIndex = 14
+        Me.btnGuardar.Text = "Guardar SQL"
+        Me.btnGuardar.UseVisualStyleBackColor = True
+        '
+        'btnModificar
+        '
+        Me.btnModificar.Location = New System.Drawing.Point(363, 271)
+        Me.btnModificar.Name = "btnModificar"
+        Me.btnModificar.Size = New System.Drawing.Size(128, 23)
+        Me.btnModificar.TabIndex = 15
+        Me.btnModificar.Text = "Editar Usuario"
+        Me.btnModificar.UseVisualStyleBackColor = True
+        '
+        'btnEliminar
+        '
+        Me.btnEliminar.Location = New System.Drawing.Point(364, 354)
+        Me.btnEliminar.Name = "btnEliminar"
+        Me.btnEliminar.Size = New System.Drawing.Size(127, 23)
+        Me.btnEliminar.TabIndex = 16
+        Me.btnEliminar.Text = "Eliminar Usuario"
+        Me.btnEliminar.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnEliminar)
+        Me.Controls.Add(Me.btnModificar)
+        Me.Controls.Add(Me.btnGuardar)
+        Me.Controls.Add(Me.btnBuscar)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.cmbComuna)
+        Me.Controls.Add(Me.txtObservaciones)
+        Me.Controls.Add(Me.txtCiudad)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtApellido)
+        Me.Controls.Add(Me.txtNombre)
+        Me.Controls.Add(Me.txtRUT)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtRUT As TextBox
+    Friend WithEvents txtNombre As TextBox
+    Friend WithEvents txtApellido As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents rbtnNoEspecifica As RadioButton
+    Friend WithEvents rbtnFemenino As RadioButton
+    Friend WithEvents rbtnMasculino As RadioButton
+    Friend WithEvents txtCiudad As TextBox
+    Friend WithEvents txtObservaciones As TextBox
+    Friend WithEvents cmbComuna As ComboBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents btnBuscar As Button
+    Friend WithEvents btnGuardar As Button
+    Friend WithEvents btnModificar As Button
+    Friend WithEvents btnEliminar As Button
+End Class
